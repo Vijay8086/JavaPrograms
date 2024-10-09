69f785d8d52abdbc55b9639b12e19e8851ad9259
Vijay <er.vijaykantpatel@gmail.com>
2024-10-09 22:19:42 +0530

    Adding code of 8 oct

---
+++ src/Oct/ex_08102024/L007.java
@@ -0,0 +1,21 @@
+package Oct.ex_08102024;
+
+public class L007 {
+
+    public static void main(String[] args){
+
+        int a = 45;
+        byte age = 22;
+        short rank = 2345;
+        long phone_number = 34534535L;
+        boolean Is_indian = true;
+        float time = 10.30F;
+        double value_of_pi = 3.14456547;
+
+        System.out.println(a);
+        System.out.println(age);
+        System.out.println(phone_number);
+
+
+    }
+}
---
+++ src/Oct/ex_08102024/L008.java
@@ -0,0 +1,15 @@
+package Oct.ex_08102024;
+
+public class L008 {
+
+    public static void main(String[] args) {
+
+        int $ = 4;
+        System.out.println($);
+
+        int _age = 5;
+        System.out.println(_age);
+
+
+    }
+}
