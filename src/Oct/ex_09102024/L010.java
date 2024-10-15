68c81c0f4e3d8bb069646c6c8e47c55417c7c8c8
Vijay <er.vijaykantpatel@gmail.com>
2024-10-15 21:52:43 +0530

    Adding code of 10 oct

---
+++ src/Oct/ex_09102024/L010.java
@@ -0,0 +1,4 @@
+package Oct.ex_09102024;
+
+public class L010 {
+}
---
+++ src/Oct/ex_10102024/L011.java
@@ -0,0 +1,10 @@
+package Oct.ex_10102024;
+
+public class L011 {
+
+    public static void main(String[] args) {
+
+        
+    }
+
+}
