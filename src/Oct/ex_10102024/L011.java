472fbdbab4c68423bd9964c5d1098efbdfcc6be7
Vijay <er.vijaykantpatel@gmail.com>
2024-10-15 22:11:03 +0530

    Adding code of 11 oct

--- src/Oct/ex_10102024/L011.java
+++ src/Oct/ex_10102024/L011.java
@@ -3,8 +3,23 @@ package Oct.ex_10102024;
 public class L011 {
 
     public static void main(String[] args) {
+        int age = 10;
+        System.out.printf("Your age is %d",age);
+        System.out.println();
 
-        
+        //%d -> int, byte, long, short
+
+        //%s -> String
+
+        // %f -> floating, double
+
+        //%b -> boolean
+
+        String name = "Vijay Patel";
+        System.out.printf("Your name is %s",name);
+
+
+
     }
 
 }
